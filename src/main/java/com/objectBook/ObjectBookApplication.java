aa97742d0c964fa54fea4b5d9d4d53a2b4598f9e
ys <chyoungsik0266@gmail.com>
2023-12-22 11:10:14 +0900

    스프링부트로 프로젝트 변경

---
+++ src/main/java/com/objectBook/ObjectBookApplication.java
@@ -0,0 +1,13 @@
+package com.objectBook;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectBookApplication.class, args);
+    }
+
+}
